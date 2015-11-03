29eeb44775e5a38906dbfcbc2440ecd8227bc596
Roshan Jobanputra <roshan@spire.com>
2015-11-03 09:55:37 -0800

    Add .gitignore and corrections to meteor package.js.

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   summary: "javascript port of predict open-source satellite tracking library",
   version: "1.0.0",
-  name: "jspredict",
+  name: "rosh93:jspredict",
   git: "https://github.com/nsat/jspredict"
 });
 
 Package.onUse(function(api) {
-  api.use("momentjs:moment");
+  api.use("momentjs:moment@2.10.6");
   api.addFiles('satellite.js', ['client', 'server'], {
     bare: true
   });
