aef0b170ca46ccfb61572219c11f917ca48fa800
Matt Molo <matt.molo@spire.com>
2021-10-15 15:57:37 -0600

    update versions

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "javascript port of predict open-source satellite tracking library",
-  version: "1.1.1",
-  name: "rosh93:jspredict",
+  version: "1.2.0",
+  name: "nsat:jspredict",
   git: "https://github.com/nsat/jspredict"
 });
 
